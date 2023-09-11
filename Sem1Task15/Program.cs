500234d36a8c5299116e96ff8d98acca5bc1b6e1
Elka Pavlova <elka@Sergeys-MacBook-Pro-2.local>
2023-09-11 20:33:53 +0300

    solution sem1 Task 10,13,15

---
+++ Sem1Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// №15 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+Console.WriteLine("Введите число от 1 до 7: ");
+int den = int.Parse(Console.ReadLine()??"0");
+
+if (den<=5)
+{
+    Console.WriteLine(den + " - рабочий");
+}
+else if (den<=7)
+{
+    Console.WriteLine(den + " - выходной");
+}
