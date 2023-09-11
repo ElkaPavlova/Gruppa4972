13044f224b56c35b3c05c226ba157298209d867e
Elka Pavlova <elka@Sergeys-MacBook-Pro-2.local>
2023-09-11 21:39:58 +0300

    Sem1 Task 14,16

--- Sem1Task14/Program.cs
+++ Sem1Task14/Program.cs
@@ -12,3 +12,7 @@ else
 {
     Console.WriteLine ("Это число не кратно 7 и 23");
 }
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(num % 7 == 0 && num % 23 == 0 ? "Кратно" : "Не кратно");
---
+++ Sem1Task16/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Задача No16
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// мое решение
+// Console.WriteLine("Введите два числа: "); 
+// int oneNum = int.Parse(Console.ReadLine());
+// int twoNum = int.Parse(Console.ReadLine());
+
+// if (oneNum == twoNum*twoNum) 
+// {
+//     Console.WriteLine("Первое число является квадратом второго");
+// }
+// else if (twoNum == oneNum*oneNum)
+// {
+//     Console.WriteLine("Второе число является квадратом первого");
+// }
+// else 
+// {
+//     Console.WriteLine("Числа не квадраты друг друга...");
+// }
+
+int numA = int.Parse(Console.ReadLine() ?? "0");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+
+bool test1 = (numA == numB*numB);
+bool test2 = (numB == numA*numA);
+
+if (test1) Console.WriteLine("Первое квадрат второго");
+if (test2) Console.WriteLine("Второе квадрат первого");
+if (!(test1||test2))  // логическая "или", если тест1 и тест2 не выполнились, то делаем то, что ниже
+{
+    Console.WriteLine("Числа не квадраты друг друга");
+}
