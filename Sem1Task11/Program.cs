9eba712232b4f7c006748629edb364bcbb099b12
Elka Pavlova <elka@Sergeys-MacBook-Pro-2.local>
2023-09-11 20:42:55 +0300

    Sem 1 Task 11,12,14

---
+++ Sem1Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выволит случайное трехзначное число и удаляет вторую цифру этого числа
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int firstDigit = num/100;
+int lastDigit = num%10;
+num = firstDigit*10+lastDigit;
+
+Console.WriteLine(num);
