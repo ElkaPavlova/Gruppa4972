2c284bd76b68fa606343d44d4fb15da84d45b44c
Elka Pavlova <elka@Sergeys-MacBook-Pro-2.local>
2023-09-25 22:51:12 +0300

    Solution from Sem5,6 Task 38, 39, 40

--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -5,12 +5,15 @@
 
 double[] GenArray(int length)
 {
-    //double[] arr = new double(length);
-    Random arr=new Random();
+    double[] arr = new double[length];
+    
     for (int i = 0; i < length; i++)
     {
         //arr[i] = Random.NextDouble(lowBorder, highBorder + 1);
-        arr[i] = Random.NextDouble();
+
+        Random rand = new Random();
+
+        arr[i] = Math.Round(rand.NextDouble()*100, 2);
     }
     return arr;
 }
@@ -20,51 +23,63 @@ void PrintArr(double[] arr) //печатаем массив
     Console.Write("[");
     for (int i=0; i < arr.Length-1; i++)
     {
-        Console.Write(arr[i] + ",");
+        Console.Write(arr[i] + "  ");
     }
 Console.WriteLine((arr[arr.Length-1]) + "]");
 }
 
-double SearchMax(double[] arr)
-{
-    double maximum = arr[0];
-    for(int i = 1; i < arr.Length; i++)
-    {
-        if(arr[i] > maximum)
-        {
-            maximum = arr[i];
-        }
-    }
-    return maximum;
-}
-// double MaxMinNumber(double[] arr)
+// double SearchMax(double[] arr)
 // {
-//     double numMax = arr[0];
-//     double numMin = arr[0];
-//     double[] newArray = [numMin; numMax];
+//     double maximum = arr[0];
 //     for(int i = 1; i < arr.Length; i++)
 //     {
-//         if(arr[i] > numMax)
+//         if(arr[i] > maximum)
 //         {
-//             numMax = arr[i];
-//         }
-//         else if (arr[i] < numMin)
-//         {
-//             numMin = arr[i];
+//             maximum = arr[i];
 //         }
 //     }
-//     //Console.WriteLine("min - " + numMin + ", max - " + numMax);
-//     return newArray;
+//     return maximum;
 // }
+double[] MaxMinNumber(double[] arr)
+{
+    double numMax = arr[0];
+    double numMin = arr[0];
+    
+    for(int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i] > numMax)
+        {
+            numMax = arr[i];
+        }
+        else if (arr[i] < numMin)
+        {
+            numMin = arr[i];
+        }
+    }
+    double[] newArray = new double[2];
+    newArray[0] = numMin;
+    newArray[1] = numMax;
+    //Console.WriteLine("min - " + numMin + ", max - " + numMax);
+    return newArray;
+}
+
+double MinMaxDiff(double min, double max)
+{
+    double diff = max - min;
+    return  Math.Round(diff, 2);
+}
 
 void PrintResult(string msg)  // печатаем результат
 {
     Console.WriteLine(msg);
 }
 
-double[] array = GenArray(6, 1, 20); // генерируем массив
+double[] array = GenArray(6); // генерируем массив
 PrintArr(array); // печатаем массив
-Console.WriteLine(SearchMax(array));
+double[] arrNew = MaxMinNumber(array);
+PrintResult("Разница между максимальным и минимальным числом - " + MinMaxDiff(arrNew[0], arrNew[1]));
+
+//Console.WriteLine(SearchMax(array));
 //Console.WriteLine("min - " + MaxMinNumber(array) + ", max - " + numMaxMaxMinNumber(array));
 //PrintResult("Сумма элементов, стоящих на нечётных позициях - "+res); // выводим результат
 
