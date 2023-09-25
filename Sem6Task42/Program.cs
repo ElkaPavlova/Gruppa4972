1ae1f37308af739bce8b9a0d896752fd7173df15
Elka Pavlova <elka@Sergeys-MacBook-Pro-2.local>
2023-09-25 23:55:42 +0300

    Solution from Sem 6 Task 42, 44, 45

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+
+string DecToBin(int num) //перевод в двоичную систему счисления
+{
+    string line = String.Empty;
+    while (num > 0)
+    {
+        line = num%2 + line;
+        num = num/2;
+    }
+
+    return line;
+}
+
+void PrintResult(string msg) // печатаем результат
+{
+    Console.WriteLine(msg);
+}
+
+int number = ReadData("Введите число: ");
+string lineNew = DecToBin(number);
+PrintResult("В двоичной системе счисления - "+lineNew);
---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+
+void PrintFibNum(int num) //выводим числа Фибоначи
+{
+    int buf = 0;
+    int first = 0;
+    int second = 1;
+    Console.Write(first + "-" + second);
+
+    for(int i = 2; i < num; i++)
+    {
+        Console.Write("-" + (first + second));
+        buf = first + second;
+        first = second;
+        second = buf;
+    }
+    Console. WriteLine();
+}
+//****************************
+
+int number = ReadData("Введите число: ");
+PrintFibNum(number);
