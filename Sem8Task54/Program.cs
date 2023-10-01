99434427780a414aa94bf61e1a0f98fdbad03c36
Elka Pavlova <elka@Sergeys-MacBook-Pro-2.local>
2023-10-01 23:31:07 +0300

    Solution from Sem 8 Task 54, 56

---
+++ Sem8Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int ReadData(string line) // вводим данные
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int[,] Fill2DArray(int countRow, int countColumn, int downBorder, int topBorder) ///заполняем массив случайными числами
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i,j] = rnd.Next(downBorder,topBorder);
+        }
+    }
+
+    return array2D;
+}
+
+void Print2DArray(int[,] matrix) // печатаем результат
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Array.Sort(intArray);
+
+void SortArr(int[,] arr) // Метод сортироовки строк в двумерном массиве
+{
+    int[] buf= new int[arr.GetLength(1)];
+
+    for (int i=0; i < arr.GetLength(0); i++)
+    {
+        for (int j=0; j < arr.GetLength(1); j++)
+        {
+            buf[j] = arr[i,j];
+        }
+        Array.Sort(buf); // Вызов стандартного метода сортироки одномерного массива
+
+        for (int j=0; j < arr.GetLength(1); j++) // Пересохранение отсортированной строки в двумерном массиве
+        {
+            arr[i,j] = buf[j];
+        }
+    }
+}
+
+int[,] arr2D = Fill2DArray(3,4,1,10);
+
+Print2DArray(arr2D);
+
+SortArr(arr2D);
+
+Console.WriteLine("\n\r");
+
+Print2DArray(arr2D);
---
+++ Sem8Task56/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] Fill2DArray(int countRow, int countColumn, int downBorder, int topBorder) ///заполняем массив случайными числами
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i,j] = rnd.Next(downBorder,topBorder);
+        }
+    }
+
+    return array2D;
+}
+
+void Print2DArray(int[,] matrix) // печатаем результат
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindMinSummLine(int[,] arr) // Метод поиска строки с наименьшей суммой элементов
+{
+    int numOfLine = 0;
+
+    int summLine = 0;
+    int summLinePrev = 0;
+
+    for (int j=0; j < arr.GetLength(1); j++) // Считаем что первая строка и есть строка с минимальной суммой элеметнов
+    {
+        summLinePrev += arr[0,j];
+    }
+
+    for (int i=1; i < arr.GetLength(0); i++)  // Сравниваем суммы элементов оставшихся строк
+    {
+        summLine = 0;
+        for (int j=0; j < arr.GetLength(1); j++)
+        {
+            summLine += arr[i,j];
+        }
+
+        if (summLine < summLinePrev) { // Если сумма сравниваемой строки элементов меньше чем предыдущая - запоминаем сумму и номер строки
+            summLinePrev = summLine;
+            numOfLine = i;
+        }
+    }
+
+    return numOfLine;
+}
+
+int[,] arr2D = Fill2DArray(5,3,1,10);
+
+Print2DArray(arr2D);
+
+Console.WriteLine("\n\r");
+
+Console.WriteLine("Номер строки с минимальной суммой значений: " + FindMinSummLine(arr2D));
